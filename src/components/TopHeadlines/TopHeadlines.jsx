3bcfc4fae5da9a7ee3671558d83af4132244963d
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-28 20:36:21 +0100

    top news route articles list

--- src/components/TopHeadlines/TopHeadlines.jsx
+++ src/components/TopHeadlines/TopHeadlines.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import classes from "./TopHeadlines.module.css";
 
 function TopHeadlines() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  const [topHeadlines, setTopHeadlines] = useState([]);
+  //methods
+  const fetchItems = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=f2a6ecdbdd0048f8bb6a2d988489bc67"
+      )
+      .then((res) => {
+        console.log(res.data.articles);
+        const data = res.data.articles;
+        setTopHeadlines(data);
+      });
+  };
+
+  let article = null;
+
+  if (topHeadlines.length > 0) {
+    article = (
+      <div>
+        {topHeadlines.map((art) => {
+          return (
+            <div className={classes.article}>
+              <img className={classes.cover} src={art.urlToImage} alt="" />
+              <h3>{art.title}</h3>
+              <p>{art.description}</p>
+              <a href={art.url}>Read More</a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Top News</h1>
+      {article}
     </div>
   );
 }
