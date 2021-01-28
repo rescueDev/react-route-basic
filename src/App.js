cf5e721a634c109279a6dc7a98a8cb724286f8b0
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-28 18:40:54 +0100

    routes created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import TopHeadlines from "./components/TopHeadlines/TopHeadlines";
+import Authors from "./components/Authors/Authors";
+import Everything from "./components/Everything/Everything";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-      <Nav />
-      <TopHeadlines />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/topnews" component={TopHeadlines} />
+        <Route path="/allnews" component={Everything} />
+        <Route path="/authors" component={Authors} />
+      </div>
+    </Router>
   );
 }
 
